b19bef54ffca6b6f5bdb865ee994f585760a9718
Rikard Andersson <mail@rikardandersson.se>
2012-05-19 13:54:08 +0200

    Changed the names of ViewControllers to better describe their functions

---
+++ BookStore/BookStore/AddBookViewController.m
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.m
+//  BookStore
+//
+//  Created by Rikard Andersson on 2012-05-19.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AddBookViewController.h"
+
+@interface AddBookViewController ()
+
+@end
+
+@implementation AddBookViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
---
+++ BookStore/BookStore/SearchBooksViewController.m
@@ -0,0 +1,34 @@
+//
+//  FirstViewController.m
+//  BookStore
+//
+//  Created by Rikard Andersson on 2012-05-19.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SearchBooksViewController.h"
+
+@interface SearchBooksViewController ()
+
+@end
+
+@implementation SearchBooksViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
