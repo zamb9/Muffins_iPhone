bfeaa13c032f29d07a0171c45e0d6c9ec4eaa76d
Rikard Andersson <mail@rikardandersson.se>
2012-05-19 13:53:16 +0200

    Added a third ViewController to act as settings

---
+++ BookStore/BookStore/SettingsViewController.m
@@ -0,0 +1,43 @@
+//
+//  SettingsViewController.m
+//  BookStore
+//
+//  Created by Rikard Andersson on 2012-05-19.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
